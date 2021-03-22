4232ac00d8290b09d484bedabd4863f869074589
Mahmudul Hasan <hasanmahmudul356@gmail.com>
2021-03-23 01:05:15 +0600

    Jquery Ui datepicker vue

---
+++ app_example_intregation.js
@@ -0,0 +1,15 @@
+//-------Other plugin--------
+
+import jQuery from 'jquery';
+require('./jquery_ui/jquery-ui');
+window.jQuery = window.$ = jQuery;
+
+//-------Other plugin--------
+
+
+
+//-------Other plugin--------
+import DatePicker from './jui-datepicker'
+Vue.component('datepicker', DatePicker);
+//-------Other plugin--------
+
